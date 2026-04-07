58c6a9eab6d67fe2d78188b38e86cb4ced6f64ad
Shreenidhi-06 <shreenidhi.23cs150@sode-edu.in>
2026-04-07 12:44:20 +0530

    Create Contact.jsx

---
+++ lab9/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+
+      <p><strong>Name:</strong> Nidhi</p>
+      <p><strong>Phone:</strong> +91 123456789</p>
+      <p><strong>Email:</strong> xxdhi@gmail.com</p>
+      <p><strong>Location:</strong> Chennai</p>
+
+    </div>
+  );
+}
+
+export default Contact;

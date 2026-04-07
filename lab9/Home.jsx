d59db7bad3f1cc3f2b38b31fb13a2792afe102bb
Shreenidhi-06 <shreenidhi.23cs150@sode-edu.in>
2026-04-07 12:44:44 +0530

    Create Home.jsx

---
+++ lab9/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Home()
+{
+  return (
+  <div>
+      <h2>Home Page</h2>;
+      <img src="./src/Image.jpg" alt="Home"/>
+      </div>
+  );
+}
+export default Home;
